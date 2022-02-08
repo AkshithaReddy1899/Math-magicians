611978c21bba742ef03feb38494d9e875cb79048
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-02-08 17:53:14 +0530

    Styling changes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
+        <p>My first react app
         </p>
         <a
           className="App-link"
